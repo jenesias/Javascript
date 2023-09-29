763a69dae88334eafae56527d72277eb0244fa2b
jenesias <114764050+jenesias@users.noreply.github.com>
2023-09-29 10:14:26 +0000

    datatypes

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js node as newer version
+
+
+// alert(3+3) we are using node js not browser
+// tc39 and mdn documentation
+
+let name = "shruti" //string datatype ("",'')
+let age = 19 //number
+let isLogin //boolean
+// null = stand alone value
+//undefined = value not assigned
+//symbol = unique
+//object  
+
